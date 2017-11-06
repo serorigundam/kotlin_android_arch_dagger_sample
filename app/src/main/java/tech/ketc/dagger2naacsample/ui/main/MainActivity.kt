f17983d6366eb36aa966992405e5739a89d83c9b
serori <serori328@gmail.com>
2017-11-07 01:43:00 +0900

    viewModel生成のユーティリティ

--- app/src/main/java/tech/ketc/dagger2naacsample/ui/main/MainActivity.kt
+++ app/src/main/java/tech/ketc/dagger2naacsample/ui/main/MainActivity.kt
@@ -1,19 +1,19 @@
 package tech.ketc.dagger2naacsample.ui.main
 
 import android.arch.lifecycle.ViewModelProvider
-import android.arch.lifecycle.ViewModelProviders
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import tech.ketc.dagger2naacsample.R
 import tech.ketc.dagger2naacsample.ui.model.MainViewModel
+import tech.ketc.dagger2naacsample.util.arch.viewmodel.viewModel
 import tech.ketc.dagger2naacsample.util.di.AutoInject
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity(), AutoInject {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
-    private val model: MainViewModel by lazy { ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java) }
+    private val model: MainViewModel by viewModel(viewModelFactory)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/tech/ketc/dagger2naacsample/util/arch/viewmodel/_AppCompatActivity.kt
@@ -0,0 +1,9 @@
+package tech.ketc.dagger2naacsample.util.arch.viewmodel
+
+import android.arch.lifecycle.ViewModel
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+
+inline fun <reified T : ViewModel> AppCompatActivity.viewModel(provider: ViewModelProvider.Factory)
+        = lazy { ViewModelProviders.of(this, provider).get(T::class.java) }
